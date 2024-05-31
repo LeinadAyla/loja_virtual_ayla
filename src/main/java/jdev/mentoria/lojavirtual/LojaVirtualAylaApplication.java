623620c211705c82ba0c3629e27805f3f88b9b14
LeynadAyla <wilsonfreitashgem@gmail.com>
2024-05-31 01:41:17 -0300

    commit inicial projeto mentoria java

---
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualAylaApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualAylaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualAylaApplication.class, args);
+	}
+
+}
